cf2e70d4e8566fd571c2b88ad6bbeeab250df5d0
ahmadkmal <eng.ahmadkmal@gmail.com>
2020-07-14 00:13:43 +0300

    Updates

---
+++ precache-manifest.98be7134fdc5ea8bef010c660e33639c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be945aba5b358de6d779e38b70129ca4",
+    "url": "/storefront/index.html"
+  },
+  {
+    "revision": "34445b96d6dc3c806d15",
+    "url": "/storefront/static/js/2.052786b0.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/storefront/static/js/2.052786b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6989be54a8d040117b5e",
+    "url": "/storefront/static/js/main.5de6e9e8.chunk.js"
+  },
+  {
+    "revision": "1a7fc9d192ce577fe91e",
+    "url": "/storefront/static/js/runtime-main.bd2b8fc7.js"
+  }
+]);
